9b7a1d9c46f6e7b8a9681c2ee8b90abd9bc38096
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-04-26 16:12:52 +0530

    Adding Login to Ecommerce

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Layout/Footer";
 import CartProvider from "./Components/Store/CartProvider";
 import Routers from "./Components/Routers/Routers";
+import { AuthContextProvider } from "./Components/Store/AuthContext";
 
 function App() {
   return (
     <Router>
       <CartProvider>
-        <Header></Header>
-        <Routers></Routers>
-        <Footer></Footer>
+        <AuthContextProvider>
+          <Header></Header>
+          <Routers></Routers>
+          <Footer></Footer>
+        </AuthContextProvider>
       </CartProvider>
     </Router>
   );
